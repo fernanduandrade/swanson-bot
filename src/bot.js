a8739a638f058613e70df90dad7b823ee085b54a
fernanduandrade <onanduandrade@gmail.com>
2022-01-14 08:19:08 -0300

    add: color command

--- src/bot.js
+++ src/bot.js
@@ -28,7 +28,23 @@ bot.on('message', async msg => {
     if(msg.content === '!quebreiprod') {
         await msg.reply('https://tenor.com/view/michael-scott-steve-carell-crying-sad-tears-gif-7910100');
     }
-    if(msg.content.includes('!git')) {
+
+    if(msg.content.includes('!color')) {
+        const color = msg.content.split(' ')[1];
+        return msg.guild.roles.create({
+            name: msg.author.tag,
+            color: color,
+            mentionable: false,
+            position: 60
+        })
+        .then(role => {
+            msg.reply(`Teste mudar de cor`)
+            msg.member.roles.add(role);
+        })
+        .catch(() => msg.reply('deu ruim'));
+    }
+
+    if(msg.content.includes('!github')) {
         msg.delete();
         let username = msg.content.split(" ")[1];
         axios.get(`https://api.github.com/users/${username}`).then(result => {
@@ -45,41 +61,6 @@ bot.on('message', async msg => {
         });
     }
 
-    if(msg.content === '!teste') {
-        msg.delete();
-        // https://fcsapi.com/api-v3/forex/base_latest?symbol=USD&type=forex&access_key=7koiEQwY5zjG3Lmo09Hc'
-        api.get('base_latest?symbol=USD&type=forex&access_key=7koiEQwY5zjG3Lmo09Hc').then(result => {
-            console.log(result.data);
-            msg.reply(`${result.data.msg}`)
-        });
-    }
-
-    if(msg.content === '!teste1') {
-        msg.delete();
-        api.get('profile?symbol=USD&access_key=7koiEQwY5zjG3Lmo09Hc').then(result => {
-            console.log(result.data);
-            msg.reply(`${result.data.msg}`)
-        })
-    }
-
-    if(msg.content === '!teste2') {
-        msg.delete();
-        api.get('economy_cal?symbol=USD,JPY&from=2022-01-11&to=2022-01-12&access_key=7koiEQwY5zjG3Lmo09Hc').then(result => {
-            console.log(result.data);
-        })
-    }
-
-    if(msg.content === '!bomdia') {
-        msg.reply('https://tenor.com/view/webamigos-bom-dia-caralho-caralho-krl-gif-21817066');
-    }
-
-    if(msg.content === '!fracasso') {
-        msg.delete();
-        api.get('history?symbol=USD/JPY&period=1h&access_key=7koiEQwY5zjG3Lmo09Hc').then(result => {
-            msg.reply(`${result.data.response['1640070000'].tm}`)
-        })
-    }
-
     if(msg.content === '!maquinadecodar') {
         msg.reply('https://cdn.discordapp.com/attachments/662667257658474519/928019803606777876/IMG_20220104_171915.jpg');
     }
@@ -87,6 +68,12 @@ bot.on('message', async msg => {
     if(msg.content === '!tchacatchacanabutchaca') {
         await msg.reply('https://tenor.com/view/skylab-defante-aplauso-gif-23138015')
     }
+
+    if(msg.content === '!bomdia') {
+        msg.reply('https://tenor.com/view/webamigos-bom-dia-caralho-caralho-krl-gif-21817066');
+    }
+
+    
 });
 
 bot.login(process.env.TOKEN);
